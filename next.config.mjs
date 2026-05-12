4e1d6ded84ef36444547468bee008d7e427aa685
Mayank Sharma <unichain0@gmail.com>
2026-05-12 10:36:29 +0530

    fix: added CSP to validate CloudFlare CAPTCHA

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,12 @@ const isDevelopment = process.env.NODE_ENV === "development";
 
 const csp = [
   "default-src 'self'",
-  `script-src 'self' 'unsafe-inline' ${isDevelopment ? "'unsafe-eval' " : ""}https://checkout.razorpay.com https://*.clerk.accounts.dev https://*.clerk.com https://clerk.mailor.gathor.online`,
-  "style-src 'self' 'unsafe-inline'",
-  "img-src 'self' data: blob: https://lh3.googleusercontent.com https://img.clerk.com",
+  `script-src 'self' 'unsafe-inline' ${isDevelopment ? "'unsafe-eval' " : ""}https://checkout.razorpay.com https://*.clerk.accounts.dev https://*.clerk.com https://clerk.mailor.gathor.online https://challenges.cloudflare.com`,
+  "style-src 'self' 'unsafe-inline' https://challenges.cloudflare.com",
+  "img-src 'self' data: blob: https://lh3.googleusercontent.com https://img.clerk.com https://challenges.cloudflare.com",
   "font-src 'self' data:",
-  "connect-src 'self' https://*.clerk.accounts.dev https://*.clerk.com https://clerk-telemetry.com https://api.razorpay.com https://checkout.razorpay.com https://www.googleapis.com https://clerk.mailor.gathor.online",
-  "frame-src 'self' https://checkout.razorpay.com https://api.razorpay.com https://*.clerk.accounts.dev https://*.clerk.com https://clerk.mailor.gathor.online",
+  "connect-src 'self' https://*.clerk.accounts.dev https://*.clerk.com https://clerk-telemetry.com https://api.razorpay.com https://checkout.razorpay.com https://www.googleapis.com https://clerk.mailor.gathor.online https://challenges.cloudflare.com",
+  "frame-src 'self' https://checkout.razorpay.com https://api.razorpay.com https://*.clerk.accounts.dev https://*.clerk.com https://clerk.mailor.gathor.online https://challenges.cloudflare.com",
   "frame-ancestors 'none'",
   "form-action 'self' https://checkout.razorpay.com https://api.razorpay.com",
   "base-uri 'self'",
